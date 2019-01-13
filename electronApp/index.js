edbc3a62b8e4838a802d3bb640662f8ae8eb746a
red3141 <red3141@gmail.com>
2019-01-12 22:45:48 -0500

    Add EOG data processing and characters laying on the ground

--- electronApp/index.js
+++ electronApp/index.js
@@ -131,21 +131,185 @@ function handleJsonApiEvent(data) {
 }
 
 var championMasteryUpdateData;
-var eogStatsBlockData;
+//var eogStatsBlockData;
+var eogStatsBlockDataProcessed = [];
 
 function handleChampionMasteryUpdates(data) {
-    console.log("ChampionMasteryUpdates: " + data);
+    //console.log("ChampionMasteryUpdates: " + data);
     championMasteryUpdateData = data;
-    if (eogStatsBlockData) {
-        draw();
-    }
+    //if (eogStatsBlockData) {
+    //    draw();
+    //}
 }
 
 function handleEogStatsBlock(data) {
     console.log("EogStatsBlock: " + data);
-    eogStatsBlockData = data;
-    if (championMasteryUpdateData) {
-        draw();
+    //eogStatsBlockData = data;
+    processEogStatsBlockData(data);
+    draw();
+    //if (championMasteryUpdateData) {
+    //    draw();
+    //}
+}
+
+function processEogStatsBlockData(data) {
+    var teams = data.data.teams;
+    var playersTeam = teams[0].isPlayerTeam ? teams[0] : teams[1];
+    var didPlayerWin = playersTeam.isWinningTeam;
+    var teamAssists = playersTeam.stats.ASSISTS;
+    var teamKills = playersTeam.stats.CHAMPIONS_KILLED;
+    var teamDeaths = playersTeam.stats.NUM_DEATHS;
+    var teamGold = playersTeam.stats.GOLD_EARNED;
+    var teamLevel = playersTeam.stats.LEVEL;
+    var teamDamageToChampions = playersTeam.stats.TOTAL_DAMAGE_DEALT_TO_CHAMPIONS;
+    var teamDamageToObjectives = playersTeam.stats.TOTAL_DAMAGE_DEALT_TO_OBJECTIVES;
+    var teamVisionScore = playersTeam.stats.VISION_SCORE;
+    
+    eogStatsBlockDataProcessed = [];
+    
+    for (var player in playersTeam.players) {
+        var processedPlayer = {};
+        processedPlayer.championId = player.championId;
+        var celebrationLevel = 0;
+        
+        var kda = 0;
+        if (player.stats.NUM_DEATHS == 0) {
+            kda = (player.stats.CHAMPIONS_KILLED + player.stats.ASSISTS);
+        } else {
+            kda = (player.stats.CHAMPIONS_KILLED + player.stats.ASSISTS) / player.stats.NUM_DEATHS;
+        }
+        if (kda > 5) {
+            celebrationLevel += 10;
+        } else if (kda > 3) {
+            celebrationLevel += 5;
+        } else if (kda > 2) {
+            celebrationLevel += 2;
+        }
+        
+        if (teamGold > 0) {
+            var fractionOfTeamsGold = player.stats.GOLD_EARNED / teamGold;
+            if (fractionOfTeamsGold > 0.4) {
+                celebrationLevel += 10;
+            } else if (fractionOfTeamsGold > 0.2) {
+                celebrationLevel += 5;
+            } else if (fractionOfTeamsGold > 0.15) {
+                celebrationLevel += 2;
+            }
+        }
+        
+        if (teamDamageToChampions > 0) {
+            var fractionOfTeamsDamageToChampions = player.stats.TOTAL_DAMAGE_DEALT_TO_CHAMPIONS / teamDamageToChampions;
+            if (fractionOfTeamsDamageToChampions > 0.4) {
+                celebrationLevel += 10;
+            } else if (fractionOfTeamsDamageToChampions > 0.2) {
+                celebrationLevel += 5;
+            } else if (fractionOfTeamsDamageToChampions > 0.15) {
+                celebrationLevel += 2;
+            }
+        }
+        
+        if (teamDamageToObjectives > 0) {
+            var fractionOfTeamsDamageToObjectives = player.stats.TOTAL_DAMAGE_DEALT_TO_OBJECTIVES / teamDamageToObjectives;
+            if (fractionOfTeamsDamageToObjectives > 0.4) {
+                celebrationLevel += 10;
+            } else if (fractionOfTeamsDamageToObjectives > 0.2) {
+                celebrationLevel += 5;
+            } else if (fractionOfTeamsDamageToObjectives > 0.15) {
+                celebrationLevel += 2;
+            }
+        }
+        
+        if (teamVisionScore > 0) {
+            var fractionOfTeamsVisionScore = player.stats.VISION_SCORE / teamVisionScore;
+            if (fractionOfTeamsVisionScore > 0.4) {
+                celebrationLevel += 10;
+            } else if (fractionOfTeamsVisionScore > 0.2) {
+                celebrationLevel += 5;
+            } else if (fractionOfTeamsVisionScore > 0.15) {
+                celebrationLevel += 2;
+            }
+        }
+        
+        if (teamLevel > 0) {
+            var fractionOfTeamsLevel = player.stats.LEVEL / teamLevel;
+            if (fractionOfTeamsVisionScore > 0.15) {
+                celebrationLevel += 5;
+            }
+        }
+        
+        if (player.stats.LARGEST_MULTI_KILL >= 5) {
+            celebrationLevel += 100;
+        }
+        
+        if (didPlayerWin) {
+            if (celebrationLevel > 100) {
+                processedPlayer.facialExpression = faceDealWithItImage;
+            } else if (celebrationLevel > 15) {
+                processedPlayer.facialExpression = faceHappyImage;
+            } else if (celebrationLevel > 0) {
+                processedPlayer.facialExpression = faceFrustratedImage;
+            } else {
+                processedPlayer.facialExpression = faceDeadImage;
+            }
+            
+            if (celebrationLevel > 35) {
+                processedPlayer.leftArmRaised = true;
+                processedPlayer.rightArmRaised = true;
+                processedPlayer.leftArm = "leftArmStraight";
+                processedPlayer.rightArm = "rightArmStraight";
+            } else if (celebrationLevel > 23) {
+                processedPlayer.leftArmRaised = false;
+                processedPlayer.rightArmRaised = false;
+                processedPlayer.leftArm = "leftArmStraight";
+                processedPlayer.rightArm = "rightArmStraight";
+            } else if (celebrationLevel > 15) {
+                processedPlayer.leftArmRaised = false;
+                processedPlayer.rightArmRaised = false;
+                processedPlayer.leftArm = "leftArmBent";
+                processedPlayer.rightArm = "rightArmBent";
+            } else if (celebrationLevel > 0) {
+                processedPlayer.leftArmRaised = false;
+                processedPlayer.rightArmRaised = false;
+                processedPlayer.leftArm = "leftArmStraight";
+                processedPlayer.rightArm = "rightArmStraight";
+            } else {
+                processedPlayer.leftArmRaised = Math.random() > 0.5;
+                processedPlayer.rightArmRaised = Math.random() > 0.5;
+                processedPlayer.leftArm = "leftArmStraight";
+                processedPlayer.rightArm = "rightArmStraight";
+            }
+            
+            if (celebrationLevel > 0) {
+                processedPlayer.layingDown = false;
+            } else {
+                processedPlayer.layingDown = true;
+            }
+        } else {
+            if (celebrationLevel > 100) {
+                processedPlayer.facialExpression = faceDealWithItImage;
+                processedPlayer.leftArmRaised = true;
+                processedPlayer.rightArmRaised = true;
+                processedPlayer.leftArm = "leftArmStraight";
+                processedPlayer.rightArm = "rightArmStraight";
+                processedPlayer.layingDown = false;
+            } else if (celebrationLevel > 35) {
+                processedPlayer.facialExpression = faceFrustratedImage;
+                processedPlayer.leftArmRaised = false;
+                processedPlayer.rightArmRaised = false;
+                processedPlayer.leftArm = "leftArmBent";
+                processedPlayer.rightArm = "rightArmBent";
+                processedPlayer.layingDown = false;
+            } else {
+                processedPlayer.facialExpression = faceDeadImage;
+                processedPlayer.leftArmRaised = Math.random() > 0.5;
+                processedPlayer.rightArmRaised = Math.random() > 0.5;
+                processedPlayer.leftArm = "leftArmStraight";
+                processedPlayer.rightArm = "rightArmStraight";
+                processedPlayer.layingDown = true;
+            }
+        }
+        
+        eogStatsBlockDataProcessed.push(processedPlayer);
     }
 }
 
@@ -198,6 +362,54 @@ function prepareImages() {
 }
 
 function draw() {
+    eogStatsBlockDataProcessed = [
+        {
+            "championId": 1,
+            "facialExpression": faceDealWithItImage,
+            "leftArmRaised": true,
+            "rightArmRaised": true,
+            "leftArm": "leftArmStraight",
+            "rightArm": "rightArmStraight",
+            "layingDown": false
+        },
+        {
+            "championId": 115,
+            "facialExpression": faceDeadImage,
+            "leftArmRaised": Math.random() > 0.5,
+            "rightArmRaised": Math.random() > 0.5,
+            "leftArm": "leftArmStraight",
+            "rightArm": "rightArmStraight",
+            "layingDown": true
+        },
+        {
+            "championId": 103,
+            "facialExpression": faceFrustratedImage,
+            "leftArmRaised": false,
+            "rightArmRaised": false,
+            "leftArm": "leftArmStraight",
+            "rightArm": "rightArmStraight",
+            "layingDown": false
+        },
+        {
+            "championId": 22,
+            "facialExpression": faceDeadImage,
+            "leftArmRaised": Math.random() > 0.5,
+            "rightArmRaised": Math.random() > 0.5,
+            "leftArm": "leftArmStraight",
+            "rightArm": "rightArmStraight",
+            "layingDown": true
+        },
+        {
+            "championId": 134,
+            "facialExpression": faceHappyImage,
+            "leftArmRaised": false,
+            "rightArmRaised": false,
+            "leftArm": "leftArmBent",
+            "rightArm": "rightArmBent",
+            "layingDown": false
+        },
+    ];
+    
     console.log("DRAWING");
     var canvas = document.getElementById("canvas");
     var context = canvas.getContext("2d");
@@ -205,16 +417,29 @@ function draw() {
 
     context.save();
     context.translate(70, 100);
-    for (var i = 0; i < 5; ++i) {
-        championId = 0;
-        if (i == 0) { championId = 1; }
-        if (i == 1) { championId = 115; }
-        if (i == 2) { championId = 103; }
-        if (i == 3) { championId = 22; }
-        if (i == 4) { championId = 134; }
-        drawCharacter(context, championId.toString());
-        context.translate(170, 0);
+    if (eogStatsBlockDataProcessed && eogStatsBlockDataProcessed.length > 0) {
+        console.log(eogStatsBlockDataProcessed);
+        for (var i = 0; i < eogStatsBlockDataProcessed.length; ++i) {
+            character = eogStatsBlockDataProcessed[i];
+            context.save();
+            if (character.layingDown) {
+                var rotateClockwise = Math.random() > 0.5;
+                // Having the upper arm raised and straight looks a little weird if the character is
+                // lying down, so prevent that.
+                if (rotateClockwise && character.rightArmRaised) {
+                    character.rightArm = "rightArmBent";
+                } else if (!rotateClockwise && character.leftArmRaised) {
+                    character.leftArm = "leftArmBent";
+                }
+                context.translate(rotateClockwise ? 180 : -80, rotateClockwise ? 200 : 300);
+                context.rotate((rotateClockwise ? 90 : -90) * Math.PI / 180);
+            }
+            drawCharacter(context, character);
+            context.restore();
+            context.translate(170, 0);
+        }
     }
+
     context.restore();
     
     championMasteryUpdateData = undefined;
@@ -318,9 +543,9 @@ function drawArmAddOn(context, armType, armRaisedCompensationRotation, addOnDeta
     context.restore();
 }
 
-function drawCharacter(context, championId) {
+function drawCharacter(context, character) {
     context.save();
-    var champion = championData[championId];
+    var champion = championData[character.championId.toString()];
     var hairColor;
     if (champion) {
         hairColor = champion.hair;
@@ -338,25 +563,20 @@ function drawCharacter(context, championId) {
     
     context.drawImage(headImage, 0, 0);
     if (!champion.hideFace) {
-        drawFacialExpression(context, championId);
+        drawFacialExpression(context, character);
     }
     drawHair(context, hairColor);
-    drawBody(context, champion, championId);
+    drawBody(context, champion, character);
     if (champion) {
         drawAddOn(context, champion.addOnAfterHead);
     }
     context.restore();
 }
 
-function drawFacialExpression(context, championId) {
+function drawFacialExpression(context, character) {
     var facialExpression = faceHappyImage;
-    var championIdInt = parseInt(championId);
-    if (championIdInt < 50) {
-        facialExpression = faceFrustratedImage;
-    } else if (championIdInt < 125) {
-        facialExpression = faceDeadImage;
-    } else if (championIdInt < 200) {
-        facialExpression = faceDealWithItImage;
+    if (character.facialExpression) {
+        facialExpression = character.facialExpression;
     }
     context.drawImage(facialExpression, 0, 0);
 }
@@ -381,31 +601,27 @@ function drawHair(context, hairColor) {
     context.restore();
 }
 
-function drawBody(context, champion, championId) {
+function drawBody(context, champion, character) {
     context.save();
     context.translate(27, headImage.height - 14);
     var hueRotationDegrees = Math.floor(Math.random() * 360);
     drawPants(context, champion);
-    drawRightSleeve(context, hueRotationDegrees, champion, championId, false);
-    drawLeftSleeve(context, hueRotationDegrees, champion, championId, false);
+    drawRightSleeve(context, hueRotationDegrees, champion, character, false);
+    drawLeftSleeve(context, hueRotationDegrees, champion, character, false);
     drawImageWithHueRotation(context, shirtImage, hueRotationDegrees);
     if (champion) {
         drawAddOn(context, champion.addOnAfterShirt);
     }
-    drawRightSleeve(context, hueRotationDegrees, champion, championId, true);
-    drawLeftSleeve(context, hueRotationDegrees, champion, championId, true);
+    drawRightSleeve(context, hueRotationDegrees, champion, character, true);
+    drawLeftSleeve(context, hueRotationDegrees, champion, character, true);
     context.restore();
 }
 
-function drawRightSleeve(context, hueRotationDegrees, champion, championId, drawAddOns) {
+function drawRightSleeve(context, hueRotationDegrees, champion, character, drawAddOns) {
     context.save();
     context.translate(-6, 1);
-    armType = "rightArmStraight";
-    if (championId < 100) {
-        // TODO: replace with an actual condition
-        //armType = "rightArmBent";
-    }
-    armRaised = false;
+    armType = character.rightArm;
+    armRaised = character.rightArmRaised;
     if (armRaised) {
         context.rotate(105 * Math.PI / 180);
         context.translate(-10, -26);
@@ -429,15 +645,11 @@ function drawRightArm(context, champion, armType, armRaised, drawAddOns) {
     context.restore();
 }
 
-function drawLeftSleeve(context, hueRotationDegrees, champion, championId, drawAddOns) {
+function drawLeftSleeve(context, hueRotationDegrees, champion, character, drawAddOns) {
     context.save();
     context.translate(shirtImage.width - 17, 0);
-    armType = "leftArmStraight";
-    if (championId < 100) {
-        // TODO: replace with an actual condition
-        //armType = "leftArmBent";
-    }
-    armRaised = false;
+    armType = character.leftArm;
+    armRaised = character.leftArmRaised;
     if (armRaised) {
         context.rotate(-105 * Math.PI / 180);
         context.translate(-20, -5);
