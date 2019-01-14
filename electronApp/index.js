c73323c4f1dbe0adde6b2af451fbde1507619ddf
red3141 <red3141@gmail.com>
2019-01-13 23:32:07 -0500

    Added Kennen

--- electronApp/index.js
+++ electronApp/index.js
@@ -389,7 +389,7 @@ function draw() {
             "layingDown": true
         },
         {
-            "championId": 429,
+            "championId": 85,
             "facialExpression": faceFrustratedImage,
             "leftArmRaised": false,
             "rightArmRaised": false,
